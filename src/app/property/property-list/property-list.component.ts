75823b6bb8395bd8248c4ec68174f0bee4b789ca
Gautam <gautammakwana9058@gmail.com>
2022-04-19 08:41:39 +0530

    Property card and list

--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-property-list',
@@ -6,6 +6,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-list.component.css']
 })
 export class PropertyListComponent implements OnInit {
+
+  properties: Array<any> = [{
+    "id" : 1,
+    "Name" : "Birla house",
+    "Type" : "House",
+    "Price" : 12000,
+
+  },
+  {
+    "id" : 2,
+    "Name" : "avd house",
+    "Type" : "villa",
+    "Price" : 12000,
+
+  },
+  {
+    "id" : 3,
+    "Name" : "eryer house",
+    "Type" : "House",
+    "Price" : 12000,
+
+  },
+  {
+    "id" : 1,
+    "Name" : "qwe house",
+    "Type" : "vilaa",
+    "Price" : 12000,
+
+  },
+  {
+    "id" : 1,
+    "Name" : "uyiuy house",
+    "Type" : "House",
+    "Price" : 12000,
+
+  },
+  {
+    "id" : 1,
+    "Name" : "bmh house",
+    "Type" : "villa",
+    "Price" : 12000,
+
+  },];
 
   constructor() { }
 
